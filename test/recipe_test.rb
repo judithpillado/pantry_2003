a9b36e6e6ac59fc469234443a419fa2131648853
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 10:36:03 -0500

    Add recipe name

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -13,5 +13,9 @@ class RecipeTest < Minitest::Test
   def test_it_exists
     assert_instance_of Recipe, @recipe1
   end
-  
+
+  def test_it_has_a_name
+    assert_equal "Mac and Cheese", @recipe1.name
+  end 
+
 end
