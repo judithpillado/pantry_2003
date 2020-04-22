dddb8ede9a225310159c766c59b72f5ab92faa95
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 11:48:41 -0500

    Add total calories test

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -11,6 +11,12 @@ class CookBookTest < Minitest::Test
     @cookbook = CookBook.new
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
+    @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
   end
 
   def test_it_exists
@@ -25,6 +31,16 @@ class CookBookTest < Minitest::Test
     @cookbook.add_recipe(@recipe1)
     @cookbook.add_recipe(@recipe2)
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
+  def test_it_can_add_total_calories
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+    assert_equal 440, @recipe1.total_calories
+    assert_equal 675, @recipe2.total_calories
   end
 
 end
