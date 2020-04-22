1373338ce4c7db4d3157be42ab36182241f67762
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 10:49:20 -0500

    Add ingredients required empty hash

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -8,6 +8,9 @@ class RecipeTest < Minitest::Test
 
   def setup
     @recipe1 = Recipe.new("Mac and Cheese")
+    @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
   end
 
   def test_it_exists
@@ -16,6 +19,10 @@ class RecipeTest < Minitest::Test
 
   def test_it_has_a_name
     assert_equal "Mac and Cheese", @recipe1.name
-  end 
+  end
+
+  def test_ingredients_required_starts_empty
+    assert_equal ({}), @recipe1.ingredients_required
+  end
 
 end
