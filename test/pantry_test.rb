cf462ea62788ef7e39363858cf1b08ac7ffee56a
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 10:21:24 -0500

    Add empty stock test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -11,5 +11,10 @@ class PantryTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Pantry, @pantry
-  end 
+  end
+
+  def test_pantry_stock
+    assert_equal ({}), @pantry.stock
+  end
+
 end
