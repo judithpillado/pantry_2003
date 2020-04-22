fe00eebd327cb19cb0d3485fe87716ada438aced
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 10:13:57 -0500

    Add attributes

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,6 +1,10 @@
 class Ingredient
+  attr_reader :name, :unit, :calories
 
   def initialize(attributes)
+    @name = attributes[:name]
+    @unit = attributes[:unit]
+    @calories = attributes[:calories]
   end
 
 end
