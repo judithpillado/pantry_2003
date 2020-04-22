28b696b45300092d2c95bab891bb244199dcec56
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 10:04:39 -0500

    Add test it exists

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -8,4 +8,8 @@ class IngredientTest < Minitest::Test
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
   end
 
+  def test_it_exists
+    assert_instance_of Ingredient, @ingredient1
+  end
+  
 end
