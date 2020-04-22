dc25c4745be83083d7703f317d61051ead66fdb3
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 11:36:12 -0500

    Add recipe test

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -3,16 +3,28 @@ require 'minitest/pride'
 require './lib/ingredient'
 require './lib/pantry'
 require './lib/recipe'
-require './lib/cookbook'
+require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
 
   def setup
     @cookbook = CookBook.new
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   def test_it_exists
     assert_instance_of CookBook, @cookbook
+  end
+
+  def test_recipes_are_empty
+    assert_equal [], @cookbook.recipes
+  end
+
+  def test_it_can_add_recipes
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
   end
 
 end
