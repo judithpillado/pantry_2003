4d319fcc35bc198bcbe12c11c6c9e27a1902aa08
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 10:29:13 -0500

    Add test it can restock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,14 +7,28 @@ class PantryTest < Minitest::Test
 
   def setup
     @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
 
   def test_it_exists
     assert_instance_of Pantry, @pantry
   end
 
-  def test_pantry_stock
+  def test_stock_starts_empty
     assert_equal ({}), @pantry.stock
+  end
+
+  def test_it_can_check_stock_of_ingredient
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+  end
+
+  def test_it_can_restock_ingredient
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+    @pantry.restock(@ingredient2, 7)
+    assert_equal 7, @pantry.stock_check(@ingredient2)
   end
 
 end
