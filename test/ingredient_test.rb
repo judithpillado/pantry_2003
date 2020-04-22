3bce67d776c0136e448a8c57529371584fe05e7c
Judith Pillado <judith.pillado@gmail.com>
2020-04-22 10:03:52 -0500

    Add setup

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -3,5 +3,9 @@ require 'minitest/pride'
 require './lib/ingredient'
 
 class IngredientTest < Minitest::Test
-  
+
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+  end
+
 end
